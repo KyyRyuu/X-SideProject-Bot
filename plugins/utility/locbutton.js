e461b49ecbe8ee7650e3021675f68ac3c2ef5511
Ryuu-tami chi <akunallbotmelky@gmail.com>
2026-06-14 20:33:21 +0900

    Upload plugins/utility/locbutton.js

---
+++ plugins/utility/locbutton.js
@@ -0,0 +1,19 @@
+export default {
+  command: ["locbutton", "locbtn"],
+  tags: ["utility"],
+  help: ["locbutton"],
+
+  async run(m, { sock }) {
+    await sock.sendButton(m.chat, {
+      title: "Saturn HQ",
+      subtitle: "Tap below",
+      text: "Location header button demo.",
+      footer: "sock.sendButton location mode",
+      location: { latitude: -6.2, longitude: 106.816666, name: "Jakarta", address: "Indonesia" },
+      buttons: [
+        { type: "reply", text: "Ping", id: ".ping" },
+        { type: "reply", text: "Menu", id: ".menu" }
+      ]
+    });
+  }
+};
