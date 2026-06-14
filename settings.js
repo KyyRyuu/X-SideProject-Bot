cc9b449ce778246dd4eb1e3fba2bd7bf0f73f466
Ryuu-tami chi <akunallbotmelky@gmail.com>
2026-06-14 20:33:31 +0900

    Upload settings.js

---
+++ settings.js
@@ -0,0 +1,48 @@
+export default {
+  botName: "X Side - Project",
+  ownerName: "IkyyKzy",
+
+  ownerNumber: ["6281248845231"],
+
+  prefix: [".", "!", "#"],
+
+  sticker: {
+    packname: "X Side",
+    author: "Baileys v7"
+  },
+
+  version: null, //gausah di apa"in
+
+  sessionName: "x-side",
+
+  connection: {
+    usePairingCode: true,
+    pairingNumber: "6281246493375",
+    browser: ["XBot", "Chrome", "120.0.0"],
+    printQR: false,
+    markOnlineOnConnect: false,
+    syncFullHistory: false,
+    reconnectDelay: 2000,
+    maxReconnectDelay: 30000
+  },
+
+  behaviour: {
+    selfIgnore: false,
+    selfMode: true,
+    groups: true,
+    autoRead: false,
+    autoTyping: false,
+    cooldown: 2000
+  },
+
+  logger: {
+    level: "info",
+    baileysLevel: "silent"
+  },
+
+  database: {
+    adapter: "json",
+    path: "./database/database.json",
+    saveInterval: 5000
+  }
+};
